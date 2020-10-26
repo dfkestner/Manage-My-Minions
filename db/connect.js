c5e3e1466a578c08427353ea59291db41842c019
Tea Flores-Kestner <dfkestner@gmail.com>
2020-10-26 18:48:06 -0400

    fixed typo

--- db/connect.js
+++ db/connect.js
@@ -1,17 +1,17 @@
 const mysql = require("mysql");
 const util = require("util");
 
-const connections = mysql.createConnection({
+const connection = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "serenity6!",
     database: "minions_db"
 });
 
-connections.connect(function(err) {
+connection.connect(function(err) {
     if (err) throw err;
 });
 
-connections.query = util.promisify(connection.query);
+connection.query = util.promisify(connection.query);
 
-module.exports = connections
+module.exports = connection
